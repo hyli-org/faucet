d94c4b6a1eb982e34be54d248109d003b0c63e8d
Bertrand Darbon <bertrand@hyle.eu>
2025-04-24 16:48:35 +0200

    ⬆️ Update Hyle

--- server/src/app.rs
+++ server/src/app.rs
@@ -1,26 +1,22 @@
-use std::{sync::Arc, time::Duration};
+use std::sync::Arc;
 
 use anyhow::Result;
 use axum::{
     extract::{Json, State},
-    http::{HeaderMap, Method, StatusCode},
+    http::Method,
     response::IntoResponse,
-    routing::{get, post},
+    routing::get,
     Router,
 };
 use client_sdk::rest_client::NodeApiHttpClient;
-use contract1::FaucetAction;
 use hyle::{
-    bus::{BusClientReceiver, BusMessage, SharedMessageBus},
     model::CommonRunContext,
     module_handle_messages,
-    rest::AppError,
     utils::modules::{module_bus_client, Module},
 };
 
-use sdk::{BlobTransaction, ContractName, TxHash};
+use sdk::ContractName;
 use serde::Serialize;
-use tokio::sync::Mutex;
 use tower_http::cors::{Any, CorsLayer};
 
 pub struct AppModule {
@@ -33,17 +29,9 @@ pub struct AppModuleCtx {
     pub faucet_cn: ContractName,
 }
 
-#[derive(Debug, Clone)]
-pub enum AppEvent {
-    SequencedTx(TxHash),
-    FailedTx(TxHash, String),
-}
-impl BusMessage for AppEvent {}
-
 module_bus_client! {
 #[derive(Debug)]
 pub struct AppModuleBusClient {
-    receiver(AppEvent),
 }
 }
 
@@ -53,10 +41,6 @@ impl Module for AppModule {
     async fn build(ctx: Self::Context) -> Result<Self> {
         let state = RouterCtx {
             faucet_cn: ctx.faucet_cn.clone(),
-            app: Arc::new(Mutex::new(HyleOofCtx {
-                bus: ctx.common.bus.new_handle(),
-            })),
-            client: ctx.node_client.clone(),
         };
 
         // Créer un middleware CORS
@@ -92,13 +76,7 @@ impl Module for AppModule {
 
 #[derive(Clone)]
 struct RouterCtx {
-    pub app: Arc<Mutex<HyleOofCtx>>,
-    pub client: Arc<NodeApiHttpClient>,
     pub faucet_cn: ContractName,
-}
-
-pub struct HyleOofCtx {
-    pub bus: SharedMessageBus,
 }
 
 async fn health() -> impl IntoResponse {
@@ -106,68 +84,13 @@ async fn health() -> impl IntoResponse {
 }
 
 // --------------------------------------------------------
-//     Headers
+//     Routes
 // --------------------------------------------------------
-
-const USER_HEADER: &str = "x-user";
-const SESSION_KEY_HEADER: &str = "x-session-key";
-const SIGNATURE_HEADER: &str = "x-request-signature";
-
-#[derive(Debug)]
-struct AuthHeaders {
-    session_key: String,
-    signature: String,
-    user: String,
-}
-
-impl AuthHeaders {
-    fn from_headers(headers: &HeaderMap) -> Result<Self, AppError> {
-        let session_key = headers
-            .get(SESSION_KEY_HEADER)
-            .and_then(|v| v.to_str().ok())
-            .ok_or_else(|| {
-                AppError(
-                    StatusCode::UNAUTHORIZED,
-                    anyhow::anyhow!("Missing session key"),
-                )
-            })?;
-
-        let signature = headers
-            .get(SIGNATURE_HEADER)
-            .and_then(|v| v.to_str().ok())
-            .ok_or_else(|| {
-                AppError(
-                    StatusCode::UNAUTHORIZED,
-                    anyhow::anyhow!("Missing signature"),
-                )
-            })?;
-
-        let user = headers
-            .get(USER_HEADER)
-            .and_then(|v| v.to_str().ok())
-            .ok_or_else(|| {
-                AppError(
-                    StatusCode::UNAUTHORIZED,
-                    anyhow::anyhow!("Missing signature"),
-                )
-            })?;
-
-        Ok(AuthHeaders {
-            session_key: session_key.to_string(),
-            signature: signature.to_string(),
-            user: user.to_string(),
-        })
-    }
-}
 
 #[derive(Serialize)]
 struct ConfigResponse {
     contract_name: String,
 }
-
-// --------------------------------------------------------
-//     Routes
-// --------------------------------------------------------
 
 async fn get_config(State(ctx): State<RouterCtx>) -> impl IntoResponse {
     Json(ConfigResponse {
