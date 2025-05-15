245a939c9afedbc0a124199aa07fc7f7acc52512
Bertrand Darbon <bertrand@hyle.eu>
2025-05-15 15:25:27 +0200

    🐛 Fix indexer url

--- front/src/services/NodeService.ts
+++ front/src/services/NodeService.ts
@@ -1,17 +1,21 @@
-import { NodeApiHttpClient } from "hyle";
+import { IndexerApiHttpClient, NodeApiHttpClient } from "hyle";
 
 class NodeService {
   client: NodeApiHttpClient;
+  indexer: IndexerApiHttpClient;
 
   constructor() {
-    this.client = new NodeApiHttpClient(import.meta.env.VITE_INDEXER_BASE_URL);
+    this.client = new NodeApiHttpClient(import.meta.env.VITE_NODE_BASE_URL);
+    this.indexer = new IndexerApiHttpClient(
+      import.meta.env.VITE_INDEXER_BASE_URL,
+    );
   }
 
   async getBalance(address: string): Promise<number> {
     interface BalanceResponse {
       balance: number;
     }
-    const balance: BalanceResponse = await this.client.get(
+    const balance: BalanceResponse = await this.indexer.get(
       "v1/indexer/contract/hyllar/balance/" + address,
       "get balance",
     );
