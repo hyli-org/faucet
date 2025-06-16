b7b46729412629b918be6d569d79a2a5e0258282
Lancelot de Ferrière <wraitii@gmail.com>
2025-06-16 20:29:22 +0200

    Fixup types

---
+++ front/src/global.d.ts
@@ -0,0 +1,11 @@
+/// src/global.d.ts
+
+declare module "react/jsx-runtime" {
+    declare namespace JSX {
+        interface IntrinsicElements {
+            "maintenance-widget": any;
+        }
+    }
+}
+
+export {};
