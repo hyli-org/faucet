4cc4dba408513514a1feb0ce84f5d8d775fb9db6
Alexandre Careil <alex.careil.fr@gmail.com>
2025-06-20 19:53:14 +0200

    fix exit process

--- server/src/main.rs
+++ server/src/main.rs
@@ -175,35 +175,8 @@ async fn main() -> Result<()> {
         })
         .await?;
 
-    #[cfg(unix)]
-    {
-        use tokio::signal::unix;
-        let mut terminate = unix::signal(unix::SignalKind::interrupt())?;
-        tokio::select! {
-            Err(e) = handler.start_modules() => {
-                error!("Error running modules: {:?}", e);
-            }
-            _ = tokio::signal::ctrl_c() => {
-                info!("Ctrl-C received, shutting down");
-            }
-            _ = terminate.recv() =>  {
-                info!("SIGTERM received, shutting down");
-            }
-        }
-        _ = handler.shutdown_modules().await;
-    }
-    #[cfg(not(unix))]
-    {
-        tokio::select! {
-            Err(e) = handler.start_modules() => {
-                error!("Error running modules: {:?}", e);
-            }
-            _ = tokio::signal::ctrl_c() => {
-                info!("Ctrl-C received, shutting down");
-            }
-        }
-        _ = handler.shutdown_modules().await;
-    }
+    handler.start_modules().await?;
+    handler.exit_process().await?;
 
     Ok(())
 }
